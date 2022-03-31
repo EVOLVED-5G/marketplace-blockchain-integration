4258fa8c2f4dfb3186b928c1a783cc479e597fb0
Paul Isaris <paulisaris@gmail.com>
2022-03-31 12:44:50 +0300

    Setting a common gas price and tip

--- send.js
+++ send.js
@@ -1,57 +1,71 @@
 const Web3 = require("web3");
 
 async function main() {
-  // Configuring the connection to an Ethereum node
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider(
-      `https://${network}.infura.io/v3/${projectId}`
-    )
-  );
-  // Creating a signing account from a private key
-  const signer = web3.eth.accounts.privateKeyToAccount(
-    privateKey
-  );
-  web3.eth.accounts.wallet.add(signer);
-  // Creating the transaction object
-  const tx = {
-    from: fromAddress,
-    to: toAddress,
-    value: web3.utils.toWei("0.0000001")
-  };
+    // Configuring the connection to an Ethereum node
+    const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+            `https://${network}.infura.io/v3/${projectId}`
+        )
+    );
+    // Creating a signing account from a private key
+    const signer = web3.eth.accounts.privateKeyToAccount(
+        privateKey
+    );
+    web3.eth.accounts.wallet.add(signer);
+    // Creating the transaction object
+    const tx = {
+        from: "0x" + fromAddress,
+        to: "0x" + toAddress,
+        value: web3.utils.toWei("0.000000001"),
+        schedule: "slow"
+    };
 
-  if(data)
-    tx.data = "0x" + Buffer.from(data, 'utf8').toString('hex');
-  // Assigning the right amount of gas
-  tx.gas = await web3.eth.estimateGas(tx);
+    if (data)
+        tx.data = "0x" + Buffer.from(data, 'utf8').toString('hex');
+    // Assigning the right amount of gas
+    tx.gas = await web3.eth.estimateGas(tx);
+    tx.maxFeePerGas = web3.utils.toWei('2.5', 'gwei');
+    tx.maxPriorityFeePerGas = web3.utils.toWei('2.5', 'gwei');
 
-  // Sending the transaction to the network
-  const receipt = await web3.eth
-    .sendTransaction(tx)
-    .once("transactionHash", (txhash) => {
-      //console.log(`Mining transaction ...`);
-      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
-    });
-  // The transaction is now on chain!
-  //console.log(`Mined in block ${receipt.blockNumber}`);
+    // console.log("base fee: " + baseFee + "\n");
+    // console.log("gas price: " + gasPrice + "\n");
+    // console.log("gas: " + tx.gas + "\n");
+    // console.log("max fee per gas: " + tx.maxFeePerGas + "\n");
+    // console.log("max priority gas price: " + tx.maxPriorityFeePerGas + "\n");
+
+    // Sending the transaction to the network
+    const receipt = await web3.eth
+        .sendTransaction(tx)
+        .once("transactionHash", (txhash) => {
+            let link = "https://";
+            if (network !== "mainnet")
+                link += network + ".";
+            link += "etherscan.io/tx/" + txhash;
+            const response = {
+                tx: tx,
+                link: link
+            }
+            process.stdout.write(JSON.stringify(response));
+        });
 }
 
 require("dotenv").config();
 const args = require('minimist')(process.argv.slice(2));
-const network = args['network'];
-if(!network)
-  throw new Error('Network not specified!');
-const projectId = args['project'];
-if(!projectId)
-  throw new Error('Project not specified!');
-const fromAddress = args['from'];
-if(!fromAddress)
-  throw new Error('From Address not specified!');
-const toAddress = args['to'];
-if(!toAddress)
-  throw new Error('To Address not specified!');
-const privateKey = args['key'];
-if(!privateKey)
-  throw new Error('Private Key not specified!');
-const data = args['data'];
+const network = args['network'].trim();
+if (!network)
+    throw new Error('Network not specified!');
+const projectId = args['project'].trim();
+if (!projectId)
+    throw new Error('Project not specified!');
+const fromAddress = args['from'].trim();
+if (!fromAddress)
+    throw new Error('From Address not specified!');
+const toAddress = args['to'].trim();
+if (!toAddress)
+    throw new Error('To Address not specified!');
+const privateKey = args['key'].trim();
+if (!privateKey)
+    throw new Error('Private Key not specified!');
+const data = args['data'].trim();
 
 main();
